7f3f78ea9f6fdc54358b6ee353baad3c23d93421
Boopathi <boopathi.it@gmail.com>
2025-05-08 14:20:02 +0530

    Update test1.sql

--- test1.sql
+++ test1.sql
@@ -1,5 +1,5 @@
 SELECT b.id, b.title, a.first_name, a.last_name
 FROM books b
-INNER JOIN authors_test_12345569999_tests_dd a
+INNER JOIN authors_test_12345569999_tests_dd_eerererererererererererererererererererererererererererererererer a
 ON b.author_id = a.id
 ORDER BY b.id
